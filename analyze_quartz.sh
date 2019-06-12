36cc75a6c320142adde32dc07a75051e97367194
Brady Lowe <lowebra2@isu.edu>
2019-06-12 12:04:29 -0600

    updated analyze_quartz.sh

--- analyze_quartz.sh
+++ analyze_quartz.sh
@@ -9,19 +9,24 @@ for run_num in ${run_list} ; do
 	detector=$(awk -F, '$1==val{print $4}' val=${run_num} values_by_run.csv)
 	signal=$(awk -F, '$1==val{print $7}' val=${run_num} values_by_run.csv)
 	if [[ ${signal} == "-1" ]] ; then
-		root -l "pmt_analyzer_pedestal.c(${run_num})"
+		#root -l "pmt_analyzer_pedestal.c(${run_num})"
+		continue
 	elif [[ ${signal} == "-2" ]] ; then
 		continue
 	elif [[ ${detector:0:5} == "mainz" || ${detector:1:3} == "am" ]] ; then
 		continue
 	elif [[ ${detector} == "tandem" ]] ; then
 		root -l "pmt_analyzer_tandem.c(${run_num})"
+		continue
 	elif [[ ${detector:3:4} == "s" ]] ; then 
-		root -l "pmt_analyzer_benchmark.c(${run_num})"
+		#root -l "pmt_analyzer_benchmark.c(${run_num})"
+		continue
 	elif [[ ${detector:3:4} == "q" ]] ; then 
 		root -l "pmt_analyzer_showermax.c(${run_num})"
+		continue
 	else
 		root -l "pmt_analyzer_showermax.c(${run_num})"
+		continue
 	fi
 	sleep 0.5
 done
